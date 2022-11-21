5c328fb307d651935a1327687ef73481f86ec179
Muhammad-Bilal-7896 <2019cs682@student.uet.edu.pk>
2022-11-21 21:32:22 +0500

    Working on first app

--- my_first_flutter_project/lib/main.dart
+++ my_first_flutter_project/lib/main.dart
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// Importing english words
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,13 +16,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
+      title: 'sd to Flutter',
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
+          title: const Text('Welcodsaf dsafme to Flutter'),
         ),
-        body: const Center(
-          child: Text('Hello World'),
+        body: Center(
+          child: Container(child: Text('Hello fgsdgdsfgfr', style: TextStyle(fontSize: 20.0)),
+          ),
         ),
       ),
     );
